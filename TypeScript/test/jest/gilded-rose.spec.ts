110cbe58c96ac2a3b9919d0846e8ec033d2df309
Julien Hougas <julien.hougas@seamless.se>
2025-12-04 08:36:14 +0100

    (test): add tests for normal item

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,56 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  describe("Normal item", () => {
+    const name = "normal item";
+    it("should be properly initiated", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+      const items = gildedRose.items;
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn);
+      expect(items[0].quality).toBe(quality);
+    });
+
+    it("should decrease quality", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality - 1);
+    });
+
+    it("quality should not be negative", () => {
+      const sellIn = 5;
+      const quality = 2;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 3);
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("sellIn negative should degrade quality twice faster", () => {
+      const sellIn = 0;
+      const quality = 5;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality - 2);
+    });
+  });
 });
