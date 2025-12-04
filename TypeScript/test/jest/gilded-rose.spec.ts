70a85eb00bf3ea87900defdb3c2398a2f2c514b2
Julien Hougas <julien.hougas@seamless.se>
2025-12-04 08:36:34 +0100

    (test): add tests for aged brie

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -58,4 +58,56 @@ describe("Gilded Rose", () => {
       expect(items[0].quality).toBe(quality - 2);
     });
   });
+
+  describe("Aged Brie", () => {
+    const name = "Aged Brie";
+    it("should be properly initiated", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+      const items = gildedRose.items;
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn);
+      expect(items[0].quality).toBe(quality);
+    });
+
+    it("should improve quality", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality + 1);
+    });
+
+    it("quality should not be more than 50", () => {
+      const sellIn = 6;
+      const quality = 49;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 3);
+      expect(items[0].quality).toBe(50);
+    });
+
+    it("sellIn negative should improve quality twice faster", () => {
+      const sellIn = 0;
+      const quality = 5;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality + 2);
+    });
+  });
 });
