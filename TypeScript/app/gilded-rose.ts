b7f5112f54f9f4dde46761ed157ce31f8dcb3927
Julien Hougas <julien.hougas@seamless.se>
2025-12-04 10:23:17 +0100

    (refactor): changed logic to switch case

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,51 +19,29 @@ export class GildedRose {
 
   updateQuality() {
     this.items.map((item) => {
-      if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
+      switch (item.name) {
+        case "Aged Brie":
+          item.sellIn--;
+          item.quality < 50 && item.quality++;
+          item.sellIn < 0 && item.quality < 50 && item.quality++;
+          break;
+
+        case "Sulfuras, Hand of Ragnaros":
+          break;
+
+        case "Backstage passes to a TAFKAL80ETC concert":
+          item.sellIn--;
+          item.quality < 50 && item.quality++;
+          item.sellIn < 10 && item.quality < 50 && item.quality++;
+          item.sellIn < 5 && item.quality < 50 && item.quality++;
+          item.sellIn < 0 && (item.quality = 0);
+          break;
+
+        default:
+          item.sellIn--;
+          item.quality > 0 && item.quality--;
+          item.sellIn < 0 && item.quality > 0 && item.quality--;
+          break;
       }
     });
 
