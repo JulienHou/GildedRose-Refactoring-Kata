42f1f121fab8ba5fd3d62c6f8b10ca2a5d6dc856
Julien Hougas <julien.hougas@seamless.se>
2025-12-04 09:07:05 +0100

    (test): add tests for backstage

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -148,4 +148,80 @@ describe("Gilded Rose", () => {
       expect(items[0].quality).toBe(quality);
     });
   });
+
+  describe("Backstage pass", () => {
+    const name = "Backstage passes to a TAFKAL80ETC concert";
+    it("should be properly initiated", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+      const items = gildedRose.items;
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn);
+      expect(items[0].quality).toBe(quality);
+    });
+
+    it("should improve quality by 1 when more than 10 days", () => {
+      const sellIn = 12;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality + 1);
+    });
+
+    it("should improve quality by 2 when between 10 and 5 days", () => {
+      const sellIn = 10;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality + 2);
+    });
+
+    it("should improve quality by when less than 5 days", () => {
+      const sellIn = 5;
+      const quality = 10;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(quality + 3);
+    });
+
+    it("quality should not be negative", () => {
+      const sellIn = 1;
+      const quality = 0;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      gildedRose.updateQuality();
+      gildedRose.updateQuality();
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 3);
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("quality should drop to 0 when sellIn <= 0", () => {
+      const sellIn = 0;
+      const quality = 30;
+      const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].name).toBe(name);
+      expect(items[0].sellIn).toBe(sellIn - 1);
+      expect(items[0].quality).toBe(0);
+    });
+  });
 });
